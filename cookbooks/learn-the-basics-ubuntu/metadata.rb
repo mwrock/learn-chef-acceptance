ddbfebff1d8e39fa52a26787d006daf5136af6e6
tpetchel <tpetchel@gmail.com>
2016-02-08 10:36:44 -0500

    update to new workflow module

--- cookbooks/learn-the-basics-ubuntu/metadata.rb
+++ cookbooks/learn-the-basics-ubuntu/metadata.rb
@@ -7,3 +7,4 @@ long_description 'Installs/Configures learn-the-basics-ubuntu'
 version '0.1.0'
 
 depends 'workflow'
+depends 'apt', '~> 2.9.2'
--- cookbooks/learn-the-basics-ubuntu/recipes/default.rb
+++ cookbooks/learn-the-basics-ubuntu/recipes/default.rb
@@ -3,7 +3,9 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
-include_recipe 'learn-the-basics-ubuntu::_setup'
-include_recipe 'learn-the-basics-ubuntu::_lesson1'
-include_recipe 'learn-the-basics-ubuntu::_lesson2'
-include_recipe 'learn-the-basics-ubuntu::_lesson3'
+Chef::Recipe.send(:include, LearnChef::Workflow)
+
+include_recipe 'learn-the-basics-ubuntu::setup'
+#include_recipe 'learn-the-basics-ubuntu::lesson1'
+#include_recipe 'learn-the-basics-ubuntu::lesson2'
+include_recipe 'learn-the-basics-ubuntu::lesson3'
